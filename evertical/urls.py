fda4e64a9bc709ca92293e5c79b83669d29b9085
Rosivaldo <js.rosivaldo@gmail.com>
2020-09-18 19:17:14 -0300

    make it better

--- evertical/urls.py
+++ evertical/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, clientes, base
+from .views import index, clientes, base, model_header, model_footer
 
 urlpatterns = [
     path('', index),
     path('clientes/', clientes),
     path('base/', base),
+    path('model_header/', model_header)
+    path('model_footer/', model_footer)
 ]
--- evertical/views.py
+++ evertical/views.py
@@ -11,3 +11,10 @@ def clientes(request):
 
 def base(request):
     return render(request, 'base,html')
+
+def model_header(request):
+    return render(request, 'model-header.html')
+
+def model_footer(request):
+    return render(request, 'model-footer.html'):
+    
