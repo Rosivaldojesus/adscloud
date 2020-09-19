d79f049daa23249afc42b5c096b032088d1352c5
Rosivaldo <js.rosivaldo@gmail.com>
2020-09-19 02:33:44 -0300

    make it better

--- evertical/migrations/0001_initial.py
+++ evertical/migrations/0001_initial.py
@@ -161,7 +161,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('clienteNome', models.CharField(blank=True, max_length=50, null=True)),
                 ('clienteAdministrador', models.CharField(blank=True, max_length=20, null=True)),
-                ('clienteAdministradora', models.CharField(blank=True, max_length=20, null=True)),
                 ('clienteCftv', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='evertical.tbSistemaCftv')),
                 ('clienteCidade', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='evertical.tbCidade')),
                 ('clienteSai', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='evertical.tbSistemaSai')),
