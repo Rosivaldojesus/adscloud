249c45062b8942d915b79980e446dfcf279f2e4c
Rosivaldo <js.rosivaldo@gmail.com>
2020-09-21 18:50:55 -0300

    make it better

--- evertical/views.py
+++ evertical/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
 def index(request):
 
-    artigo = tbArtigos.objects.reverse()[2:]
+    artigo = tbArtigos.objects.all()[3:]
 
     return render(request, 'index.html', {'artigo':artigo})
 
